c720ee0061bdaf4fa0dbc9fcec5dedebec063a1a
dhkdxh <popsicle0404@naver.com>
2025-09-15 12:48:27 +0900

    0915-2

--- src/callableStatement/MemberDAO.java
+++ src/callableStatement/MemberDAO.java
@@ -120,19 +120,23 @@ public class MemberDAO {
         String sql = "{CALL SP_MEMBER_CHECK(?)}";
 
         try(CallableStatement cs = conn.prepareCall(sql)){
+            conn.setAutoCommit(false);
             cs.setString(1, "apple");
 
             try(ResultSet rs = cs.executeQuery()) {
-
-            }
-            conn.setAutoCommit(false);
-
-            if(rs.next()){
-
-            }
+                if(rs.next()){
+                    System.out.println("m_seq: "+rs.getInt(1));
+                    System.out.println("m_userid: "+rs.getString(2));
+                    System.out.println("m_pwd: "+rs.getString(3));
+                    System.out.println("m_email: "+rs.getString(4));
+                    System.out.println("m_hp: "+rs.getString(5));
+                    System.out.println("m_registdate: "+rs.getDate(6));
+                    System.out.println("m_point: "+rs.getInt(7));
+                }
+            }catch(SQLException e){try{conn.rollback();}catch(SQLException e1){e.printStackTrace();}}
 
         }catch(SQLException e){
-
+            try{conn.rollback();}catch(SQLException e1){e.printStackTrace();}
         }
     }
 }
